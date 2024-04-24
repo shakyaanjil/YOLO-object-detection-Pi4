c4e6f9b8d7e14d838f8557ae4f599099a0473ca2
anjil <shakya.anjil01@gmail.com>
2024-04-24 11:57:50 +0900

    image filter function initialized

--- data_preprocessing/segmentation.py
+++ data_preprocessing/segmentation.py
@@ -1,9 +1,12 @@
 import os
 import cv2
+import shutil
 
 image_folder = 'C:/Users/Anjil/Documents/UnAnnotated1'
 annotation_folder = 'C:/Users/Anjil/Documents/UnAnnotated1'
 output_base_folder = 'C:/Users/Anjil/Documents/pi_phase1/segmented_images' #directory to save segmented images
+
+no_annotation_folder = 'C:/Users/Anjil/Documents/UnAnnotated2' #directory to save images with no annotations
 
 
 def segmentation():
@@ -30,6 +33,17 @@ def segmentation():
                     cropped_image = image[y_min:y_max, x_min:x_max]
                     output_folder = os.path.join(output_base_folder, class_names[int(class_id)])
                     cv2.imwrite(os.path.join(output_folder, f'{os.path.splitext(filename)[0]}_{class_id}.jpg'), cropped_image)
+            
+def filter_image():
+    os.makedirs(no_annotation_folder, exist_ok=True)
+    image_files = set([f for f in os.listdir(image_folder) if f.endswith('.jpg') or f.endswith('.png')])
+    annotation_files = set([f.replace('.txt', '.jpg') for f in os.listdir(annotation_folder) if f.endswith('.txt')])
+    images_without_annotations = image_files - annotation_files
+    for image in images_without_annotations:
+        original_path = os.path.join(image_folder, image)
+        new_path = os.path.join(no_annotation_folder, image)
+        shutil.move(original_path, new_path)  
 
 segmentation()
+filter_image()
 
