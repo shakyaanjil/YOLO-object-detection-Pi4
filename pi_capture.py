c45c71bb81e308ee90da4b1c0a0c2658b48a0f5f
anjil <shakya.anjil01@gmail.com>
2024-04-30 15:54:36 +0900

    yolo model loaded

--- pi_capture.py
+++ pi_capture.py
@@ -1,4 +1,11 @@
 import cv2
+from cvlib.object_detection import YOLO
+
+weights = "C:/Users/Anjil/Documents/pi_phase1/4ClassWeights/yolov4-tiny-obj_best.weights"
+config = "C:/Users/Anjil/Documents/pi_phase1/yolov4-tiny-custom.cfg"
+labels_path = "C:/Users/Anjil/Documents/pi_phase1/obj.names"
+
+yolo = YOLO(weights, config, labels_path)
 
 cap = cv2.VideoCapture(0)  
 
@@ -6,6 +13,11 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+    bbox, labels, confs = yolo.detect_objects(frame)
+    for box, label, conf in zip(bbox, labels, confs):
+        x, y, w, h = box
+        yolo.draw_bbox(frame, [box], [label], [conf])
+
     cv2.imshow('frame:', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
