79d087eff83e6c15a53c6c0678682952683e2fed
anjil <shakya.anjil01@gmail.com>
2024-05-10 14:54:44 +0900

    robot motor module initialized

---
+++ RobotMotor/MotorModule.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from time import sleep, time
+import threading
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+class Motor():
+    def __init__(self,EnaA,In1A,In2A,EnaB,In1B,In2B):
+        self.EnaA = EnaA
+        self.In1A = In1A
+        self.In2A = In2A
+        self.EnaB = EnaB
+        self.In1B = In1B
+        self.In2B = In2B
+        GPIO.setup(self.EnaA,GPIO.OUT)
+        GPIO.setup(self.In1A,GPIO.OUT)
+        GPIO.setup(self.In2A,GPIO.OUT)
+        GPIO.setup(self.EnaB,GPIO.OUT)
+        GPIO.setup(self.In1B,GPIO.OUT)
+        GPIO.setup(self.In2B,GPIO.OUT)
+        self.pwmA = GPIO.PWM(self.EnaA, 100);
+        self.pwmA.start(0);
+        self.pwmB = GPIO.PWM(self.EnaB, 100);
+        self.pwmB.start(0);
+        self.last_stop_time = 0
+        self.start_delay = 20  # Delay in seconds before allowing the motor to start again
+        self.allow_start = True
