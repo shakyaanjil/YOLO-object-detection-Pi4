cdee70f613bf6e291fb4c75d8c2e99bb0e91449c
anjil <shakya.anjil01@gmail.com>
2024-05-15 17:16:44 +0900

    motor module

--- RobotMotor/MotorModule.py
+++ RobotMotor/MotorModule.py
@@ -71,3 +71,13 @@ class Motor():
     def enable_start(self):
         print("Motor start allowed")
         self.allow_start = True  # Allow motor start
+
+def main():
+    motor.move(0.6,0,2)
+    motor.stop(2)
+    motor.move(-0.5,0,2)
+    motor.stop(2)
+
+if __name__ == '__main__':
+    motor= Motor(2,3,4,17,22,27)
+    main()
