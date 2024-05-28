ee2635fa976febe2084cdff4e72e1131714bdb65
shakyanjil <shakya.anjil01@gmail.com>
2024-05-28 13:59:01 +0900

    move endpoint added

--- test.py
+++ test.py
@@ -11,3 +11,15 @@ motor = Motor(2, 3, 4, 17, 22, 27)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/move/<direction>')
+def mov(direction):
+    if direction == 'up':
+        motor.move(0.4, 0, 0.1)
+    elif direction == 'down':
+        motor.move(-0.4, 0, 0.1)
+    elif direction == 'left':
+        motor.move(0.3, 0.5, 0.1)
+    elif direction == 'right':
+        motor.move(0.3, -0.5, 0.1)
+    return 'Moved ' + direction
