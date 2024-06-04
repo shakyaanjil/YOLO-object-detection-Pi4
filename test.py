e4b3d97171974b441d54e3d9c7142f7ca72a71b0
shakyanjil <shakya.anjil01@gmail.com>
2024-06-04 16:19:16 +0900

    integrating yolo detector in main app

--- test.py
+++ test.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 
 motor = Motor(2, 3, 4, 17, 22, 27)
 
+# Initialize YOLOv4 object detector
+yolo_weights = "yolov4-tiny-obj_best.weights"
+yolo_config = "yolov4-tiny-custom.cfg"
+yolo_labels = "obj.names"
+target_labels = ['Human', 'Forklift', 'Cone', 'Pallet']  # Adjust this list based on your specific targets
+object_detector = ObjectDetector(yolo_weights, yolo_config, yolo_labels, target_labels)
+
 @app.route('/')
 def index():
     return render_template('index.html')
